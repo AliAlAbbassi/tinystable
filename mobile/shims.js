956266be1d808f2ceafd7a5daaf6bf0012b847d5
Cuthlehoop <alialabbassi2001@gmail.com>
2025-10-27 10:47:26 +0200

    fix pollyfil config

--- mobile/shims.js
+++ mobile/shims.js
@@ -1,21 +1,2 @@
 // Import crypto polyfill before anything else
 import 'react-native-get-random-values';
-
-// Polyfill Buffer
-if (typeof Buffer === 'undefined') {
-  global.Buffer = require('buffer').Buffer;
-}
-
-// Polyfill process
-if (typeof process === 'undefined') {
-  global.process = require('process');
-}
-
-// Polyfill import.meta
-if (typeof globalThis !== 'undefined' && !globalThis.import) {
-  globalThis.import = {
-    meta: {
-      url: 'https://localhost:8081',
-    },
-  };
-}
